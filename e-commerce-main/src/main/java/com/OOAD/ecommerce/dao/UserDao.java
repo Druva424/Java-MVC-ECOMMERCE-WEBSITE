855f8417f7b7e5e16553083cfc33a777955b2243
Divyam Vijay <76161684+divyam2807@users.noreply.github.com>
2023-04-28 17:20:24 +0530

    Delete e-commerce-main directory

--- e-commerce-main/src/main/java/com/OOAD/ecommerce/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.OOAD.ecommerce.dao;
-
-import com.OOAD.ecommerce.entity.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserDao extends CrudRepository<User, String> {
-}
