bd32eb76a57cbc8d320abea1d0322275affa5ffe
Divyam Vijay <76161684+divyam2807@users.noreply.github.com>
2023-04-28 17:15:13 +0530

    Add files via upload

---
+++ e-commerce-main/src/main/java/com/OOAD/ecommerce/dao/UserDao.java
@@ -0,0 +1,9 @@
+package com.OOAD.ecommerce.dao;
+
+import com.OOAD.ecommerce.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDao extends CrudRepository<User, String> {
+}
